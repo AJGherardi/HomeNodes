9209913939577dda517054f6e6802580f7fb0015
Alexander Gherardi <alexander.2003@live.com>
2020-10-01 09:25:36 -0400

    Add scene recall node

--- home-node.js
+++ home-node.js
@@ -4,17 +4,32 @@ var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cook
     return cooked;
 };
 var cross_fetch_1 = require("cross-fetch");
-// import gql from 'graphql-tag';
-// import { gql } from "apollo-boost";
 var client_1 = require("@apollo/client");
-var SET_STATE = client_1.gql(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n    mutation SetState($addr: String!,$value: String!) {\n        setState(addr: $addr, value: $value)\n    }\n"], ["\n    mutation SetState($addr: String!,$value: String!) {\n        setState(addr: $addr, value: $value)\n    }\n"
-    // addr: "AgA=",
-])));
-// addr: "AgA=",
+var SET_STATE = client_1.gql(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n    mutation SetState($addr: String!,$value: String!) {\n        setState(addr: $addr, value: $value)\n    }\n"], ["\n    mutation SetState($addr: String!,$value: String!) {\n        setState(addr: $addr, value: $value)\n    }\n"])));
+var SCENE_RECALL = client_1.gql(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n    mutation SceneRecall($addr: String!, $sceneNumber: String!) {\n        sceneRecall(addr: $addr, sceneNumber: $sceneNumber) \n    }\n"], ["\n    mutation SceneRecall($addr: String!, $sceneNumber: String!) {\n        sceneRecall(addr: $addr, sceneNumber: $sceneNumber) \n    }\n"])));
 var uri = 'http://localhost:8080/graphql';
 var link = new client_1.HttpLink({ uri: uri, fetch: cross_fetch_1["default"] });
 var nodeInit = function (RED) {
-    function HomeNode(config) {
+    function SceneRecallNode(config) {
+        RED.nodes.createNode(this, config);
+        var operation = {
+            query: SCENE_RECALL,
+            variables: {
+                addr: config.addr,
+                sceneNumber: config.sceneNumber
+            }
+        };
+        this.on("input", function (msg, send, done) {
+            client_1.execute(link, operation).subscribe({
+                next: function (data) {
+                    // msg.payload = data
+                    // send(msg)
+                }
+            });
+            done();
+        });
+    }
+    function SetStateNode(config) {
         RED.nodes.createNode(this, config);
         var operation = {
             query: SET_STATE,
@@ -34,7 +49,8 @@ var nodeInit = function (RED) {
             });
         });
     }
-    RED.nodes.registerType("home-node", HomeNode);
+    RED.nodes.registerType("set-state", SetStateNode);
+    RED.nodes.registerType("scene-recall", SceneRecallNode);
 };
-var templateObject_1;
+var templateObject_1, templateObject_2;
 module.exports = nodeInit;
--- home-node.ts
+++ home-node.ts
@@ -7,13 +7,43 @@ const SET_STATE = gql`
         setState(addr: $addr, value: $value)
     }
 `
+
+const SCENE_RECALL = gql`
+    mutation SceneRecall($addr: String!, $sceneNumber: String!) {
+        sceneRecall(addr: $addr, sceneNumber: $sceneNumber) 
+    }
+`
+
 const uri = 'http://localhost:8080/graphql';
 const link = new HttpLink({ uri, fetch: fetch });
 
-interface HomeNodeDef extends NodeDef { addr: String, value: String }
+interface SetStateNodeDef extends NodeDef { addr: String, value: String }
+interface SceneRecallNodeDef extends NodeDef { addr: String, sceneNumber: String }
 
 const nodeInit: NodeInitializer = (RED): void => {
-    function HomeNode(this: Node, config: HomeNodeDef) {
+    function SceneRecallNode(this: Node, config: SceneRecallNodeDef) {
+        RED.nodes.createNode(this, config);
+        const operation = {
+            query: SCENE_RECALL,
+            variables: {
+                addr: config.addr,
+                sceneNumber: config.sceneNumber
+            },
+        };
+
+        this.on("input", (msg, send, done) => {
+            execute(link, operation).subscribe({
+                next: data => {
+                    msg.payload = data
+                    send(msg)
+                    done();
+                },
+                complete: () => console.log(`complete`),
+            })
+        });
+    }
+
+    function SetStateNode(this: Node, config: SetStateNodeDef) {
         RED.nodes.createNode(this, config);
         const operation = {
             query: SET_STATE,
@@ -33,9 +63,9 @@ const nodeInit: NodeInitializer = (RED): void => {
                 complete: () => console.log(`complete`),
             })
         });
-
     }
 
-    RED.nodes.registerType("home-node", HomeNode);
+    RED.nodes.registerType("set-state", SetStateNode);
+    RED.nodes.registerType("scene-recall", SceneRecallNode);
 }
 export = nodeInit
